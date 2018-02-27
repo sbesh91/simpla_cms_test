fb8dd1fbb57d396ade096b3df2fdc2784459d50c
sbesh91 <s.beshensky@gmail.com>
2018-02-26 18:24:18 -0600

    move headers

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const PolymerWebpackLoader = require('polymer-webpack-loader');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require("webpack");
 
 module.exports = {
@@ -29,6 +30,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.htm'
     }),
+    new CopyWebpackPlugin([
+      {from: 'bower_components', to: 'out/bower_components'},
+      {from: 'server', to: 'out'}, 
+    ]), 
     new webpack.IgnorePlugin(/vertx/)
   ]
 };
